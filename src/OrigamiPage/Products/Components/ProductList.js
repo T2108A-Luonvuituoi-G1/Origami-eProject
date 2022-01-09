b426c735c34cffa824b074a78c6ab54b1144f38f
Minh <quangminh8699@gmail.com>
2022-01-09 16:20:20 +0700

    Create ProductList.js
    Products-demo

---
+++ src/OrigamiPage/Products/Components/ProductList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ProductList extends Component {
+    render() {
+        return (
+            <div>
+                Day la danh sach san pham
+            </div>
+        )
+    }
+}
