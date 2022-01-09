f13858157fe616eb998b9e28da2bc1d589c15a24
Minh <quangminh8699@gmail.com>
2022-01-09 16:19:51 +0700

    Create More.js
    More-demo

---
+++ src/OrigamiPage/More/Components/More.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class More extends Component {
+    render() {
+        return (
+            <div>
+                Day la gioi thieu them ve team
+            </div>
+        )
+    }
+}
