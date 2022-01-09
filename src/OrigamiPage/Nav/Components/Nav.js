de2c105ccc788a1ffac4cfbf24652572d7bf2ab9
Minh <quangminh8699@gmail.com>
2022-01-10 00:22:19 +0700

    Update Nav.js
    update Nav

--- src/OrigamiPage/Nav/Components/Nav.js
+++ src/OrigamiPage/Nav/Components/Nav.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom';
-import '../Css/Nav.css'
+import { Link } from 'react-router-dom';
+import '../Assets/Css/Nav.css'
+import Logo from '../Assets/Images/Logo.png'
 
 export default class Nav extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
     render() {
         return (
             <div>
-                <div className="topnav">
-                    <Link className="active" to="/">Home</Link>
-                    <Link to="/products">Products</Link>
-                    <Link to="/aboutUs">AboutUs</Link>
-                    <Link to="/contact">Contact</Link>
-                    <Link to="/more">More</Link>
+                <div className="top-nav">
+                    {/* <img className='nav-logo' src={Logo} /> */}
+                    <input type="text" className='input-search' placeholder='Search' value={this.state.value} onChange={this.handleChange} />
+                    <Link className='nav-link' to="/">Home</Link>
+                    <Link className='nav-link' to="/products">Products</Link>
+                    <Link className='nav-link' to="/aboutUs">AboutUs</Link>
+                    <Link className='nav-link' to="/contact">Contact</Link>
+                    <Link className='nav-link' to="/more">More</Link>
                 </div>
             </div>
         )
