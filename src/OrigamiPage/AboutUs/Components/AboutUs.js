e5360145c0a0cdbb59b3c031af66fc4e1463754f
Minh <quangminh8699@gmail.com>
2022-01-09 16:19:27 +0700

    Create AboutUs.js
    AboutUs-demo

---
+++ src/OrigamiPage/AboutUs/Components/AboutUs.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AboutUs extends Component {
+    render() {
+        return (
+            <div>
+                day la gioi thieu ve team
+            </div>
+        )
+    }
+}
