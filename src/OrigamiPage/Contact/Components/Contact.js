6edd98d0e7eb74e4326c3210a15d3049249da7eb
Minh <quangminh8699@gmail.com>
2022-01-09 16:19:38 +0700

    Create Contact.js
    Contact-demo

---
+++ src/OrigamiPage/Contact/Components/Contact.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Contact extends Component {
+    render() {
+        return (
+            <div>
+                day la cach lien lac voi team
+            </div>
+        )
+    }
+}
