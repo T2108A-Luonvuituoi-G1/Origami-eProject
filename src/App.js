270179ccf055ad77e6c4df8228a82118e1f7cae6
Minh <quangminh8699@gmail.com>
2022-01-09 16:18:58 +0700

    Update App.js
    Update router function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-function App() {
-  return (
-    <>
-    </>
-  );
-}
+import React, { Component } from 'react'
+import ProductList from './OrigamiPage/Products/Components/ProductList';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AboutUs from './OrigamiPage/AboutUs/Components/AboutUs';
+import Contact from './OrigamiPage/Contact/Components/Contact';
+import More from './OrigamiPage/More/Components/More';
+import Home from './OrigamiPage/Home/Components/Home';
+import Nav from './OrigamiPage/Nav/Components/Nav';
 
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Nav />
+          <Routes>
+            <Route exact path="/" element={ <Home />} />
+            <Route exact path="/products" element={ <ProductList />} />
+            <Route exact path="/aboutUs" element={ <AboutUs />} />
+            <Route exact path="/contact" element={ <Contact />} />
+            <Route exact path="/more" element={ <More />} />
+          </Routes>
+          
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
