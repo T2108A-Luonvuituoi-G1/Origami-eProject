3404f3c4c765b45a188f039bd6c2e4f7ed164331
Minh <quangminh8699@gmail.com>
2022-01-10 15:45:42 +0700

    Create UserProfile.js

---
+++ src/OrigamiPage/UserProfile/Components/UserProfile.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class UserProfile extends Component {
+    render() {
+        return (
+            <div>
+                Day la profile cua nguoi dung
+            </div>
+        )
+    }
+}
