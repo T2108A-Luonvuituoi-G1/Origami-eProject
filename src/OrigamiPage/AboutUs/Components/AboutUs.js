2adf5b265ba0f06550af2f677ef8a0d9ed33867d
Minh <quangminh8699@gmail.com>
2022-01-11 11:21:05 +0700

    Update AboutUs.js

--- src/OrigamiPage/AboutUs/Components/AboutUs.js
+++ src/OrigamiPage/AboutUs/Components/AboutUs.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import { BiAlarmExclamation } from "react-icons/bi";
+import '../Assets/Css/AboutUs.css'
 
 export default class AboutUs extends Component {
     render() {
         return (
             <div>
+                <BiAlarmExclamation />
                 day la gioi thieu ve team
             </div>
         )
