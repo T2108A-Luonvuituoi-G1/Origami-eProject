4bfcea77d3045a2f44788897e6eccad8d965f453
Minh <quangminh8699@gmail.com>
2022-01-11 11:21:08 +0700

    Update Contact.js

--- src/OrigamiPage/Contact/Components/Contact.js
+++ src/OrigamiPage/Contact/Components/Contact.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import { BiAccessibility } from "react-icons/bi";
 
 export default class Contact extends Component {
     render() {
         return (
             <div>
+                <BiAccessibility />
                 day la cach lien lac voi team
             </div>
         )
