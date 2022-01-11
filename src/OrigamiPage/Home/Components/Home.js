2f46c2dddbc5ee45a56d02fbba3c6520678da97c
Minh <quangminh8699@gmail.com>
2022-01-11 18:25:43 +0700

    Update Home.js

--- src/OrigamiPage/Home/Components/Home.js
+++ src/OrigamiPage/Home/Components/Home.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
+import '../Assets/Css/Home.css'
 
 export default class Home extends Component {
     render() {
         return (
-            <div>
-                Day la home
+            <div className='home-page'>
+                <p>a</p>
+                <p>a</p>
+                <p>a</p>
+                <p>a</p>
+                <p>a</p>
+                <p>a</p>
+                <p>a</p>
+                <p>a</p>
+                <p>a</p>
+                <p>a</p>
+                <p>a</p>
+                <p>a</p>
+                <p>a</p>
+                <p>a</p>
+                <p>a</p>
+                <p>a</p>
+                <p>a</p>
+                <p>a</p>
             </div>
         )
     }
