725da8107d99c5c356c9db64d159503fd69d5e3a
Minh <quangminh8699@gmail.com>
2022-01-12 19:54:47 +0700

    Update Home.js

--- src/OrigamiPage/Home/Components/Home.js
+++ src/OrigamiPage/Home/Components/Home.js
@@ -1,29 +1,15 @@
 import React, { Component } from 'react'
 import '../Assets/Css/Home.css'
+import HomeBackgroundHeader from '../Assets/Images/HomeBackgroundHeader.jpg'
 
 export default class Home extends Component {
     render() {
         return (
-            <div className='home-page'>
-                <p>a</p>
-                <p>a</p>
-                <p>a</p>
-                <p>a</p>
-                <p>a</p>
-                <p>a</p>
-                <p>a</p>
-                <p>a</p>
-                <p>a</p>
-                <p>a</p>
-                <p>a</p>
-                <p>a</p>
-                <p>a</p>
-                <p>a</p>
-                <p>a</p>
-                <p>a</p>
-                <p>a</p>
-                <p>a</p>
-            </div>
+            <>
+                <div className='home-page'>
+                    <img src={HomeBackgroundHeader} className='home-bg-header' />
+                </div>
+            </>
         )
     }
 }
