012f5e2ea89d860aec610145da134120c81f4e61
Vovanviet <vietvvth2107012@fpt.edu.vn>
2022-01-12 13:14:05 +0700

    product

--- src/OrigamiPage/Products/Components/ProductList.js
+++ src/OrigamiPage/Products/Components/ProductList.js
@@ -1,11 +1,63 @@
 import React, { Component } from 'react'
+import ImageDino from '../Assets/Images/khung-long.gif'
+import DinoDetails from '../Assets/Images/gap-khung-long.gif'
+import CatDetails from '../Assets/Images/cat.jpg'
+import Cat from '../Assets/Images/cat-represent.jpg'
+import Dog from '../Assets/Images/dog-represent.jpg'
+import DogDetails from '../Assets/Images/dog-making.png'
+import react from 'react'
+import '../Assets/Css/product.css'
 
 export default class ProductList extends Component {
+    
     render() {
         return (
-            <div>
-                Day la danh sach san pham
-            </div>
+            <>{/* Making A Dino */}
+                <div className='product-item'>
+                    
+                        <img className='product-item-represent'  src={ImageDino}  />
+                        <div className='product-item-des'>
+                            <p><h1>Make Dino</h1>
+                                Categories:Children
+
+                            </p>
+                            <br/><br/>
+
+                            <a href={DinoDetails} className='product-btn-item-making' >Making</a>
+                        </div>
+                </div>    
+                {/* Making A Cat */}
+                <div className='product-item'>
+                    
+                        <div className='product-item-des'>
+                            <p><h1>Make A Cat</h1>
+                                Categories:Children
+
+                            </p>
+                            <br/><br/>
+
+                            <a href={CatDetails} className='product-btn-item-making' >Making</a>
+                        </div>
+                        <img className='product-item-represent'  src={Cat}  />
+                </div>      
+                {/* Making A Dog */}     
+                <div className='product-item'>
+                    
+                    <img className='product-item-represent'  src={Dog}  />
+                    <div className='product-item-des'>
+                        <p><h1>Make A Cat</h1>
+                            Categories:Children
+
+                        </p>
+                        <br/><br/>
+
+                        <a href={DogDetails} className='product-btn-item-making' >Making</a>
+                    </div>
+            </div>  
+
+
+            </>
+
         )
     }
 }
