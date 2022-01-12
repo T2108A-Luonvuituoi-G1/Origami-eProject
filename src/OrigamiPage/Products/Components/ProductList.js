660af101233048ec858321ad7776338c3d3f8d10
Minh <quangminh8699@gmail.com>
2022-01-12 20:32:36 +0700

    Update ProductList.js

--- src/OrigamiPage/Products/Components/ProductList.js
+++ src/OrigamiPage/Products/Components/ProductList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 import CatDetails from '../Assets/Images/making/cat.jpg'
 import Cat from '../Assets/Images/represent/cat-represent.jpg'
@@ -12,17 +13,20 @@ import Sumo from '../Assets/Images/represent/sumo-represent.jpg'
 // import Cactus from '../Assets/Images/represent/cactus-represent.jpg'
 // import Bird from '../Assets/Images/represent/bird-represent.jpg'
 
-
-
 import react from 'react'
 import '../Assets/Css/product.css'
+import ProductPlant from './ProductPlant'
 
 export default class ProductList extends Component {
-    
+
+
     render() {
         return (
             <>
- 
+                <div className='product-nav-header'>
+                    <Link className='product-nav-animal' to="/products/plant">chu so</Link>
+                </div>
+                <div className='product-content'></div>
             </>
 
         )
