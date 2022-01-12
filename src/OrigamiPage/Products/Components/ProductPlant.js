aaae9d4f6bee1f17e0777790643fb55dec4d3dc3
Minh <quangminh8699@gmail.com>
2022-01-12 20:32:38 +0700

    Update ProductPlant.js

---
+++ src/OrigamiPage/Products/Components/ProductPlant.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ProductPlant extends Component {
+    render() {
+        return (
+            <div>
+                day la muc plant
+            </div>
+        )
+    }
+}
