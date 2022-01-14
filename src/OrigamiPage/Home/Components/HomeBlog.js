b894d19751e1e0c28c64c0d87fea5d7ab8406d27
Minh <quangminh8699@gmail.com>
2022-01-14 21:06:55 +0700

    Create HomeBlog.js

---
+++ src/OrigamiPage/Home/Components/HomeBlog.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import '../Assets/Css/HomeBlog.css'
+
+export default class HomeBlog extends Component {
+    render() {
+        return (
+            <div className='home-blog-page'>
+                
+            </div>
+        )
+    }
+}
+
