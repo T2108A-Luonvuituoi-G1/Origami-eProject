86a7e8d29ecbc60023e633db11768f16a21f3329
Vovanviet <vietvvth2107012@fpt.edu.vn>
2022-01-17 22:01:51 +0700

    Update AboutUs.js

--- src/OrigamiPage/AboutUs/Components/AboutUs.js
+++ src/OrigamiPage/AboutUs/Components/AboutUs.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { BiAlarmExclamation } from "react-icons/bi";
-import '../Assets/Css/AboutUs.css'
+import '../Assets/Css/aboutUs.css'
 
 export default class AboutUs extends Component {
     render() {
