1956f24e285f120e126065827e29c1ed9c647456
tranminhduc <tranminhduc05012003@gmail.com>
2022-01-18 14:42:53 +0700

    Update UserProfile.js

--- src/OrigamiPage/UserProfile/Components/UserProfile.js
+++ src/OrigamiPage/UserProfile/Components/UserProfile.js
@@ -1,31 +1,33 @@
 import React, { Component } from 'react'
+import '../Assets/Css/UserProfile.css'
+import img1 from '../Assets/Images/Gap-giay-origami.jpg'
 
 export default class UserProfile extends Component {
     render() {
         return (
             <div>
                 <div class="Product-top">
-      <div class="card">
-          <img src="/image/chim1.jpg" alt="cover">
-    
-      </div>
-      <div> 
-          <p><b>Trần Minh Đức</b></p>
-      </div>
+                    <div class="card">
+                        <img src={img1} alt="cover" />
 
-  </div>
-  <div class="Product-bottom_left">
-      <div class="text">
-          <p>day la 1 doan text</p> 
-          <br/>
-          <p>day cung la 1 doan text</p>
-      </div>
-  </div>
-  <div class="Product-bottom_right">
-      <div class="text">
-          <p>day la 1 doan text khac </p>
-      </div>
-  </div>
+                    </div>
+                    <div>
+                        <p><b>Trần Minh Đức</b></p>
+                    </div>
+
+                </div>
+                <div class="Product-bottom_left">
+                    <div class="text">
+                        <p>day la 1 doan text</p>
+                        <br />
+                        <p>day cung la 1 doan text</p>
+                    </div>
+                </div>
+                <div class="Product-bottom_right">
+                    <div class="text">
+                        <p>day la 1 doan text khac </p>
+                    </div>
+                </div>
             </div>
         )
     }
