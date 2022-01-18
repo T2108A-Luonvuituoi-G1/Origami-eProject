1c86a3b28677621141756fd667b2b360a64fed10
daothanhnam <88824678+daothanhnam@users.noreply.github.com>
2022-01-18 14:28:30 +0700

    Update UserProfile.js

--- src/OrigamiPage/UserProfile/Components/UserProfile.js
+++ src/OrigamiPage/UserProfile/Components/UserProfile.js
@@ -4,7 +4,28 @@ export default class UserProfile extends Component {
     render() {
         return (
             <div>
-                Day la profile cua nguoi dung
+                <div class="Product-top">
+      <div class="card">
+          <img src="/image/chim1.jpg" alt="cover">
+    
+      </div>
+      <div> 
+          <p><b>Trần Minh Đức</b></p>
+      </div>
+
+  </div>
+  <div class="Product-bottom_left">
+      <div class="text">
+          <p>day la 1 doan text</p> 
+          <br/>
+          <p>day cung la 1 doan text</p>
+      </div>
+  </div>
+  <div class="Product-bottom_right">
+      <div class="text">
+          <p>day la 1 doan text khac </p>
+      </div>
+  </div>
             </div>
         )
     }
