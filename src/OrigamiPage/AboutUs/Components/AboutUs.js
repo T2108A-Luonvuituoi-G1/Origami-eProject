c60d5d17040a662a5c86cf7768bf8c88fb16bd8b
Minh <quangminh8699@gmail.com>
2022-01-19 17:10:49 +0700

    Update AboutUs.js

--- src/OrigamiPage/AboutUs/Components/AboutUs.js
+++ src/OrigamiPage/AboutUs/Components/AboutUs.js
@@ -1,104 +1,24 @@
 import React, { Component } from "react";
-import '../Assets/Css/aboutUs.css'
-import Minh from '../Assets/Images/Minhavt.jpg'
-import Viet from '../Assets/Images/Vietavt.jpg'
-import Duc from '../Assets/Images/Ducavt.jpg'
-import Vanh from '../Assets/Images/vanhavt.jpg'
-import MrT from '../Assets/Images/Mr.T.jpg'
-import Nam from '../Assets/Images/Namavt.jpg'
+import AboutUsHeader from "./AboutUsHeader";
+import AboutUsSlider from "./AboutUsSlider";
+import AboutUsContainer from "./AboutUsContainer";
+import AboutUsFooter from "./AboutUsFooter";
 
 export default class AboutUs extends Component {
   render() {
     return (
       <>
-        <div className="about_us">
-          <div className="container">
-            <h1 className="Title"> LuonVuiTuoi Team </h1>
-            <section className="about_us--top">
-              <div className="card">
-                <img src={Minh} alt="avatar" />
-                <div>
-                  <p>Ta Quang Minh</p>
-                  <a
-                    href="https://www.facebook.com/minh8699"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Leader
-                  </a>
-                </div>
-              </div>
+        {/* HEADER */}
+        <AboutUsHeader />
 
-              <div className="card">
-                <img src={Nam} alt="avatar" />
-                <div>
-                  <p>Dao Thanh Nam</p>
-                  <a
-                    href="https://www.facebook.com/thanhnam092/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Member
-                  </a>
-                </div>
-              </div>
-            </section>
-            <section className="about_us--bottom">
-              <div className="card">
-                <img src={Duc} alt="avatar" />
-                <div>
-                  <p>Tran Minh Duc</p>
-                  <a
-                    href="https://www.facebook.com/bac.soi.1297"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Member
-                  </a>
-                </div>
-              </div>
+        {/* SLIDER */}
+        <AboutUsSlider />
 
-              <div className="card">
-                <img src={Vanh} alt="avatar" />
-                <div>
-                  <p>Le Nhu Viet Anh</p>
-                  <a
-                    href="https://www.facebook.com/profile.php?id=100059104665311"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Member
-                  </a>
-                </div>
-              </div>
+        {/* CONTAINER */}
+        <AboutUsContainer />
 
-              <div className="card">
-                <img src={Viet} alt="avatar" />
-                <div>
-                  <p>Vo Van Viet</p>
-                  <a
-                    href="https://www.facebook.com/VoVanViet.2003"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Member
-                  </a>
-                </div>
-              </div>
-            </section>
-          </div>
-        </div>
-        <div className="about_teacher">
-          <div className="container">
-            <h1 className="Title">Special Advisors</h1>
-            <div className="card">
-              <img src={MrT}    alt="avatar" />
-              <div>
-                <p>Mr.Tuan Nguyen</p>
-              </div>
-            </div>
-          </div>
-        </div>
+        {/* FOOTER */}
+        <AboutUsFooter />
       </>
     );
   }
