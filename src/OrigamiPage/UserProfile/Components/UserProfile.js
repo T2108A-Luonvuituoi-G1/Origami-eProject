320852ae98c3f957df3ce27f8f009f0e7bec44ee
tranminhduc <tranminhduc05012003@gmail.com>
2022-01-20 15:24:43 +0700

    Update UserProfile.js

--- src/OrigamiPage/UserProfile/Components/UserProfile.js
+++ src/OrigamiPage/UserProfile/Components/UserProfile.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react'
 import '../Assets/Css/UserProfile.css'
 import img1 from '../Assets/Images/Anh-chim-hac.jpg'
 
-import img1 from '../Assets/Images/chim1.jpg'
-
 export default class UserProfile extends Component {
     render() {
         return (
@@ -18,12 +16,7 @@ export default class UserProfile extends Component {
                         <p><b>Trần Minh Đức</b></p>
                     </div>
 
-                </div>
-                <div classname="Profile-bottom_left">
-                    <div class="text">
-                        <p>Phan hoi voi chung toi</p>
-                    
-                    </div>
+               
                 </div>
                 <div className="Profile-bottom_right">
                     <div class="text">
@@ -32,11 +25,11 @@ export default class UserProfile extends Component {
                 <div className="Profile-bottom_left">
                     <div className="text">
                         <p>day la 1 doan text</p>
-                        <br />
-                        <p>day cung la 1 doan text</p>
+                        
+                    
                     </div>
                 </div>
-                <div className="Profile-bottom_right">
+                <div className="Profile-bottom_hight">
                     <div className="text">
                         <p>day la 1 doan text khac </p>
                     </div>
