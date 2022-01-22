949dc4f149aafbaa841b847bd3995ed62cb36ebe
Vovanviet <vietvvth2107012@fpt.edu.vn>
2022-01-22 20:33:04 +0700

    Update Frame.js

--- src/OrigamiPage/Products/Components/Frame.js
+++ src/OrigamiPage/Products/Components/Frame.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import bg from '../Assets/Images/background.jpg'
 import fr from '../Assets/Images/making/Frame1.jpg'
 import fr2 from '../Assets/Images/making/Frame2.jpg'
 import fr3 from '../Assets/Images/making/Frame3.jpg'
@@ -10,40 +11,45 @@ import React, { Component } from 'react';
 export default class Frame extends Component {
   render() {
     return (
+      <div   style={{
+        background: `url(${bg})`
+    }}>
 
-      <div class="frame">
-        <div class="frame_top">
-          <span class="frame_title">Frame Picture</span>
-          <div class="frame_title-author">
-            <span>Posed by</span>
-            <span>
-              <a
-                href="https://www.facebook.com/minh8699"
-                rel="author"> Ta Quang Minh
-              </a>
-            </span>
+        <div class="frame"
+        >
+          <div class="frame_top">
+            <span class="frame_title">Frame Picture</span>
+            <div class="frame_title-author">
+              <span>Posed by</span>
+              <span>
+                <a
+                  href="https://www.facebook.com/minh8699"
+                  rel="author"> Ta Quang Minh
+                </a>
+              </span>
+            </div>
+            <div class="frame_content">
+              <p ><strong>Form name:</strong> Frame Picture</p>
+              <p >
+                <strong>Author:</strong> Nguyen Xuan Tung
+              </p>
+            </div>
+            <div class="frame_origami">
+              <img class="f_images" src={fr} alt="image" />
+              <img class="f_images" src={fr2} alt="image" />
+              <img class="f_images" src={fr3} alt="image" />
+              <img class="f_images" src={fr4} alt="image" />
+            </div >
+            <div class="frame_bottom">
+              <p>@2022 LuonVuiTuoi Team</p> <br />
+
+            </div>
           </div>
-          <div class="frame_content">
-            <p ><strong>Tên mẫu:</strong> Frame Picture</p>
-            <p >
-              <strong>Tác giả:</strong> Nguyễn Xuân Tùng
-            </p>
-          </div>
-          <div class="frame_origami">
-            <img class="f_images" src={fr} alt="image"/>
-              <img class="f_images" src={fr2} alt="image"/>
-                <img class="f_images" src={fr3} alt="image"/>
-                  <img class="f_images" src={fr4} alt="image"/>
-                  </div >
-                  <div class="frame_bottom">
-                    <p>@2022 LuonVuiTuoi Team</p> <br />
-                    <p>Bây giờ là 12h33'</p>
-                  </div>
-                </div>
 
-              </div>
+        </div>
 
 
-              )
+      </div>
+    )
   }
 }
