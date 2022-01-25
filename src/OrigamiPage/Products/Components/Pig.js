7e8120e6eb8fbf11d5f8d365c47ea139c4ac8a48
tranminhduc <tranminhduc05012003@gmail.com>
2022-01-25 12:50:47 +0700

    Update Pig.js

--- src/OrigamiPage/Products/Components/Pig.js
+++ src/OrigamiPage/Products/Components/Pig.js
@@ -1,12 +1,44 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import React, { Component } from 'react';
+import bg from '../Assets/Images/background.jpg'
+import Bm from '../Assets/Images/making/Boar_DoTriKhai.jpg'
+import Bm2 from '../Assets/Images/making/boar.png'
 
 export default class Pig extends Component {
   render() {
     return (
-        <>
-        
-        </>
+      <div div   style={{
+        background: `url(${bg})`
+    }}>
+        <div className="boar">
+          <div className="boar_top">
+            <span className="boar_title">Boar</span>
+            <div className="boar_title-author">
+              <span>Posed by </span>
+              <span>
+                <a
+                  href="https://www.facebook.com/bac.soi.1297"
+                  rel="author"
+                >Tran Minh Duc</a>
+              </span>
+            </div>
+            <div className="boar_content">
+              <p >
+                <strong>Form name:</strong> Lợn rừng</p>
+              <p >
+                <strong>Form name:</strong> Đỗ Trí Khải</p>
+            </div>
+            <div className="boar_origami">
+              <img className="b_images" src={Bm} /> <br />
+              <img className="b_images" src={Bm2} alt="image" />
+            </div>
+            <div class="boar_bottom">
+              <p>@2022 LuonVuiTuoi Team</p> <br />
+           
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
