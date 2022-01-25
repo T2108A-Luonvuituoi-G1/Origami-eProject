eb4bc00bc4fa7d0aa833252516441a9e98e5b5f7
Minh <quangminh8699@gmail.com>
2022-01-25 21:20:16 +0700

    Create ProductNav.js

---
+++ src/OrigamiPage/Products/Components/ProductNav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../Assets/Css/ProductNav.css'
+
+function ProductNav() {
+    return (
+        <>
+            <div className='product-nav'>
+                hello
+            </div>
+        </>
+    );
+}
+
+export default ProductNav;
