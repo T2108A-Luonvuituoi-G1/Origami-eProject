318dcf774fb86a4365a1d534e30b50cb9bce6bbc
tranminhduc <tranminhduc05012003@gmail.com>
2022-01-25 12:50:32 +0700

    Update Crab.js

--- src/OrigamiPage/Products/Components/Crab.js
+++ src/OrigamiPage/Products/Components/Crab.js
@@ -1,12 +1,47 @@
 import { Link } from "react-router-dom";
 import React, { Component } from 'react';
+import bg from '../Assets/Images/background.jpg'
+import Cm from '../Assets/Images/making/tutorial.png'
+import Cm1 from '../Assets/Images/making/Crab.jpg'
 
 export default class Crab extends Component {
   render() {
     return (
-        <>
-            
-        </>
+      <div  style={{
+        background: `url(${bg})`
+    }}>
+        <div className="crab"
+        >
+          <div className="crab_top">
+            <span className="crab_title">Crab</span>
+            <div className="crab_title-author">
+              <span>Posed by</span>
+              <span>
+                <a href="https://www.facebook.com/VoVanViet.2003" rel="author">
+                  Vo Van Viet</a
+                >
+              </span>
+            </div>
+            <div className="crab_content">
+              <p ><strong>Tên mẫu:</strong>Crab</p>
+              <p >
+                <strong>Author:</strong> Nguyen Hung Cuong<br/>
+                Develop from muscle
+                version in the “Cube Art” series by Kunihiko Kasahara
+              </p>
+            </div>
+            <div className="crab_origami">
+              <img className="crab_images"
+                src={Cm1} alt="iamges" />
+              <span>Nguyen Hung Cuong- Inspired by Kunihiko Kasahara's Cube Art</span>
+            </div>
+            <img className="crab_images" src={Cm} alt="images" />
+          </div>
+          <div className="crab_bottom">
+            <p>@2022 LuonVuiTuoi Team</p>
+          </div>
+        </div>
+      </div>
     )
   }
 }
