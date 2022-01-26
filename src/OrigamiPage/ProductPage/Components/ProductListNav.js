8e67c03231e5489584161f1d29fc6f63fe30ebf2
Vovanviet <vietvvth2107012@fpt.edu.vn>
2022-01-26 22:09:55 +0700

    Update ProductListNav.js

--- src/OrigamiPage/ProductPage/Components/ProductListNav.js
+++ src/OrigamiPage/ProductPage/Components/ProductListNav.js
@@ -1,28 +1,29 @@
 import React from 'react';
 import '../Assets/Css/ProductListNav.css'
 import { BiChevronRight } from "react-icons/bi";
+import { Link } from 'react-router-dom';
 
 function ProductListNav() {
     return (
         <>
             <div className='product-page-nav'>
                 <div className='product-page-nav-link'>
-                    <a href='#' className='product-page-nav-btn'>
+                    <Link to="/products/ProductAnimal"  className='product-page-nav-btn'>
                         <p>Animal</p>
                         <BiChevronRight className='product-page-nav-icon' />
-                    </a>
-                    <a href='#' className='product-page-nav-btn'>
+                    </Link>
+                    <Link to="/products/ProductPlant" className='product-page-nav-btn'>
                         <p>Plant</p>
                         <BiChevronRight className='product-page-nav-icon' />
-                    </a>
-                    <a href='#' className='product-page-nav-btn'>
+                    </Link>
+                    <Link to="/products/ProductItem" className='product-page-nav-btn'>
                         <p>Item</p>
                         <BiChevronRight className='product-page-nav-icon' />
-                    </a>
-                    <a href='#' className='product-page-nav-btn'>
+                    </Link>
+                    <Link to="/products/ProductFlag" className='product-page-nav-btn'>
                         <p>Flag</p>
                         <BiChevronRight className='product-page-nav-icon' />
-                    </a>
+                    </Link>
                 </div>
             </div>
         </>
