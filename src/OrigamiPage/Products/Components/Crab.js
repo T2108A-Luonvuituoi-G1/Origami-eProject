daf94a1cb0921f50f7f630dab524af7fcf4550d7
tranminhduc <tranminhduc05012003@gmail.com>
2022-01-26 15:10:12 +0700

    Update Crab.js

--- src/OrigamiPage/Products/Components/Crab.js
+++ src/OrigamiPage/Products/Components/Crab.js
@@ -1,8 +1,9 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import React, { Component } from 'react';
 import bg from '../Assets/Images/background.jpg'
-import Cm from '../Assets/Images/making/tutorial.png'
-import Cm1 from '../Assets/Images/making/Crab.jpg'
+import Cm1 from '../Assets/Images/represent/crab.jpg'
+import Cm2 from '../Assets/Images/making/tutorial-crab.jpg'
+// import Cm1 from '../Assets/Images/represent/crab.jpg'
 
 export default class Crab extends Component {
   render() {
@@ -31,11 +32,11 @@ export default class Crab extends Component {
               </p>
             </div>
             <div className="crab_origami">
-              <img className="crab_images"
-                src={Cm1} alt="iamges" />
+              {/* <img className="crab_images"
+                src={Cm1} className='img-crab' alt="images" /> */}
               <span>Nguyen Hung Cuong- Inspired by Kunihiko Kasahara's Cube Art</span>
             </div>
-            <img className="crab_images" src={Cm} alt="images" />
+            <img className="crab_images" src={Cm2} alt="images" />
           </div>
           <div className="crab_bottom">
             <p>@2022 LuonVuiTuoi Team</p>
