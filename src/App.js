878c35a2f559e55865de466f3840faf5eb4a34bb
Minh <quangminh8699@gmail.com>
2022-01-26 20:37:35 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
-import ProductList from './OrigamiPage/Products/Components/ProductList';
+import ProductPage from './OrigamiPage/ProductPage/Components/ProductPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AboutUs from './OrigamiPage/AboutUs/Components/AboutUs';
 import Contact from './OrigamiPage/Contact/Components/Contact';
@@ -18,9 +18,6 @@ import Frame from './OrigamiPage/Products/Components/Frame';
 import JapanFlag from './OrigamiPage/Products/Components/JapanFlag';
 
 
-
-
-
 export default class App extends Component {
   render() {
     return (
@@ -32,22 +29,22 @@ export default class App extends Component {
           <div className='Content'>
             <Routes>
               <Route exact path="/" element={<Home />} />
-              <Route exact path="/products" element={<ProductList />} />
+              <Route exact path="/products" element={<ProductPage />} />
               <Route exact path="/aboutus" element={<AboutUs />} />
               <Route exact path="/contact" element={<Contact />} />
               <Route exact path="/profile" element={<UserProfile />} />
 
 
 
-              <Route exact path="/products/butter" element={<Butter />}/>
+              <Route exact path="/products/butterfly" element={<Butter />}/>
               <Route exact path="/products/lotus" element={<Lotus />}/>
               <Route exact path="/products/parrot" element={<Parrot />}/>
-              <Route exact path="/products/vietnam" element={<Vietnam />}/>
+              <Route exact path="/products/VietNamFlag" element={<Vietnam />}/>
               <Route exact path="/products/crab" element={<Crab />}/>
               <Route exact path="/products/pig" element={<Pig />}/>
               <Route exact path="/products/fox" element={<Fox />}/>
-              <Route exact path="/products/frame" element={<Frame />}/>
-              <Route exact path="/products/jpflag" element={<JapanFlag />}/>
+              <Route exact path="/products/frameImg" element={<Frame />}/>
+              <Route exact path="/products/JapanFlag" element={<JapanFlag />}/>
 
      
 
