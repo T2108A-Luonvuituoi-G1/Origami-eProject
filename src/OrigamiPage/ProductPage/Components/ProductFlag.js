4c87ec9e1e216dfbf51fa22ecdda64b63dc9401a
Vovanviet <vietvvth2107012@fpt.edu.vn>
2022-01-26 22:09:44 +0700

    Create ProductFlag.js

---
+++ src/OrigamiPage/ProductPage/Components/ProductFlag.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProductListNav from './ProductListNav';
+
+function ProductFlag() {
+    return (
+        <>
+            <ProductListNav/>
+        </>
+    );
+}
+
+export default ProductFlag;
