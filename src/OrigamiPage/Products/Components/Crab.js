2ca1f89d2547cbb965bf9c70063068bd1849625c
Vovanviet <vietvvth2107012@fpt.edu.vn>
2022-02-10 14:39:30 +0700

    Update Crab.js

--- src/OrigamiPage/Products/Components/Crab.js
+++ src/OrigamiPage/Products/Components/Crab.js
@@ -1,8 +1,9 @@
 // import { Link } from "react-router-dom";
 import React, { Component } from 'react';
 import bg from '../Assets/Images/background.jpg'
-import Cm1 from '../Assets/Images/represent/crab.jpg'
+// import Cm1 from '../Assets/Images/represent/crab.jpg'
 import Cm2 from '../Assets/Images/making/tutorial-crab.jpg'
+import Comment from './Comment';
 // import Cm1 from '../Assets/Images/represent/crab.jpg'
 
 export default class Crab extends Component {
@@ -37,6 +38,10 @@ export default class Crab extends Component {
               <span>Nguyen Hung Cuong- Inspired by Kunihiko Kasahara's Cube Art</span>
             </div>
             <img className="crab_images" src={Cm2} alt="images" />
+          </div>
+          <div className="comment">
+            <p className="p-text-making">Facebook comments</p>
+            <Comment />
           </div>
           <div className="crab_bottom">
             <p>@2022 LuonVuiTuoi Team</p>
