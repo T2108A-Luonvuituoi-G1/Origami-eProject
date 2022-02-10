7366fc733f3026a5e54c44c77090765f1ab84bd6
tranminhduc <tranminhduc05012003@gmail.com>
2022-02-10 14:36:25 +0700

    Update UserProfileContent.js

--- src/OrigamiPage/UserProfile/Components/UserProfileContent.js
+++ src/OrigamiPage/UserProfile/Components/UserProfileContent.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import '../Assets/Css/UserProfileContent.css'
+import { FaGithub, FaEnvelope } from "react-icons/fa";
+import { ImMail } from "react-icons/im";
+import { ImPhone } from "react-icons/im";
+import {MdSchool } from "react-icons/md";
+import { BsTwitter } from "react-icons/bs";
+import { ImFacebook } from "react-icons/im";
+import { AiFillHeart} from "react-icons/ai";
 
+{/* Mo dau */}
 function UserProfileContent() {
     return (
         <>
@@ -23,6 +31,41 @@ function UserProfileContent() {
                         <li>Posts</li>
                         <li>Follower</li>
                         <li>Following</li>
+                    </ul>
+                </div>
+
+{/*Icons */}                
+                <div className='up-icons'>
+                    <ul className='up-icons-infromation'>
+
+
+                        <li>
+                            <MdSchool /> THPT Tam Nong .
+                        </li>
+                         <li>
+                            <ImMail /> Tran****@gmail.com.
+                        </li>
+                        <li>
+                            <ImPhone /> 0352946702.
+                        </li>
+                        <li>
+                            <FaEnvelope /> Send me a message.
+                        </li>
+                        <li>
+                            <FaGithub /> LuonVuiTuoi-origami.
+                        </li>
+                        <li>
+                            <BsTwitter /> tranduc0501.                        
+                        </li>
+                        <li>
+                            <ImFacebook /> Tran Duc.
+                        </li>
+                        <li>
+                            <AiFillHeart /> Dating hihi.
+                        </li>
+                        
+                        
+                       
                     </ul>
                 </div>
             </div>
