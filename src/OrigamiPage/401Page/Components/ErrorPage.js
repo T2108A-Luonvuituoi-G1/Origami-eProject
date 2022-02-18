33653be1ef2667fec9586ca5d53a3579391bf038
vietanhle123 <93577622+vietanhle123@users.noreply.github.com>
2022-02-18 22:55:50 +0700

    update error page

--- src/OrigamiPage/401Page/Components/ErrorPage.js
+++ src/OrigamiPage/401Page/Components/ErrorPage.js
@@ -8,12 +8,10 @@ function ErrorPage() {
       <br/>
 
           <p className='ErrorPage'>401</p>
-          <h2 className='ErrorPage-Details'>The page you are trying to access could not be loaded until you first log in with a valid user ID and password !!!</h2>
+          <h2 className='ErrorPage-Details'>he page you are trying to access could not be loaded until you first log in with a valid user ID and password !!!</h2>
       <br/> 
 
-      <div className='ErrorPage_2'>
-        <a className='error-link' href="/">Back Home</a> 
-      </div>
+      <a className='nav-link' href="/">Back Home</a> 
 
     </div> 
   )
