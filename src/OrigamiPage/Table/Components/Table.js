70c1bdf4a3b8e95ee007bbb703b5c97fe81a6618
daothanhnam <88824678+daothanhnam@users.noreply.github.com>
2022-02-21 21:30:37 +0700

    Create Table.js

---
+++ src/OrigamiPage/Table/Components/Table.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import AdminNav from '../../Admin/Components/AdminNav'
+import '../Assets/Css/Table.css'
+
+function Table() {
+  return (
+    <>
+    <div>
+        <AdminNav />
+    </div>
+    <div></div>
+    </>
+  )
+}
+
+export default Table
