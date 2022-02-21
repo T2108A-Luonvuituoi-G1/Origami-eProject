72700a4a17580e7d2580d9aab3442fa1e1af0dcd
Vovanviet <vietvvth2107012@fpt.edu.vn>
2022-02-21 12:20:25 +0700

    Update BaseAvatars.js

--- src/OrigamiPage/Admin/Components/BaseAvatars.js
+++ src/OrigamiPage/Admin/Components/BaseAvatars.js
@@ -3,44 +3,61 @@ import Namavt from '../Assets/Images/Namavt.jpg'
 import '../Assets/Css/BaseAvatars.css'
 import AdminNav from '../Components/AdminNav'
 import '../Assets/Css/BaseStatus.css'
-import'../Assets/Css/BaseShape.css'
+import '../Assets/Css/BaseShape.css'
+import '../Assets/Css/BaseAvtGroup.css'
+import { Link } from 'react-router-dom'
+
 export default function BaseAvatars() {
     return (
-        <>  
-            <AdminNav/>
-            <div className='BA-Sizing'>
-                <h4 className='Base-Text'>Sizing</h4><hr />
-                <div className='BA-avartar'>
-                    <img src={Namavt} className='BA-S1' />
-                    <img src={Namavt} className='BA-S2' />
-                    <img src={Namavt} className='BA-S3' />
-                    <img src={Namavt} className='BA-S4' />
-                    <img src={Namavt} className='BA-S5' />
-                    <img src={Namavt} className='BA-S6' />
-                </div>
-            </div>
-
-
-            <div className='BA-Status'>
-                <h4 className='Base-Text'>Status Indicator</h4><hr />
-
-                <div className='BA-Status-images'>
-                    <div className='BA-Status-online '>
-                        <img src={Namavt} className='BA-Status-img' />
-                    </div>
-                    <div className='BA-Status-offline'>
-                    <img src={Namavt} className='BA-Status-img' />
-                    </div>
-                    <div className='BA-Status-away'>
-                    <img src={Namavt} className='BA-Status-img' />
+        <>
+            
+            
+            <div className='layout-Base'>
+                <div className='BA-Sizing'>
+                    <h4 className='Base-Text'>Sizing</h4><hr />
+                    <div className='BA-avartar'>
+                        <img src={Namavt} className='BA-S1' />
+                        <img src={Namavt} className='BA-S2' />
+                        <img src={Namavt} className='BA-S3' />
+                        <img src={Namavt} className='BA-S4' />
+                        <img src={Namavt} className='BA-S5' />
+                        <img src={Namavt} className='BA-S6' />
                     </div>
                 </div>
+
+
+                <div className='BA-Status'>
+                    <h4 className='Base-Text'>Status Indicator</h4><hr />
+
+                    <div className='BA-Status-images'>
+                        <div className='BA-Status-online '>
+                            <img src={Namavt} className='BA-Status-img' />
+                        </div>
+                        <div className='BA-Status-offline'>
+                            <img src={Namavt} className='BA-Status-img' />
+                        </div>
+                        <div className='BA-Status-away'>
+                            <img src={Namavt} className='BA-Status-img' />
+                        </div>
+                    </div>
+                </div>
+                <div className='BA-Shape'>
+                    <h4 className='Base-Text'>Shape</h4><hr/>
+                    <img src={Namavt} className='BA-Shape-img shape-1' />
+                    <img src={Namavt} className='BA-Shape-img shape-2' />
+                </div>
+                <div className='BA-Group'>
+                    <h4 className='Base-Text'>Group</h4><hr/>
+                    <div className='Group-img'>
+                    <img src={Namavt} className='BA-Group-img Group-img-1' />
+                    <img src={Namavt} className='BA-Group-img Group-img-2' />
+                    <img src={Namavt} className='BA-Group-img Group-img-3' />
+                    <span className='BA-Group-img span-img'>CF</span>
+                    </div>
+                </div>
+     
             </div>
-            <div className='BA-Shape'>
-                <h4 className='Base-Text'>Shape</h4>
-                <img src={Namavt} className='BA-Status-img' />
-                <img src={Namavt} className='BA-Status-img' />
-            </div>
+
         </>
     )
 }
