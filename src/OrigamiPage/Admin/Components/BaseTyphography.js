5534f1d3bfe1c26e740b97a6291508497c95fcb9
daothanhnam <88824678+daothanhnam@users.noreply.github.com>
2022-02-21 21:30:16 +0700

    Create BaseTyphography.js

---
+++ src/OrigamiPage/Admin/Components/BaseTyphography.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import '../Assets/Css/BaseTypho.css'
+
+export default function BaseTyphography() {
+    return (
+        <>
+           
+                    <table className='card card-typo'>
+                        <p className='card-head'>Card Typho</p><hr/>
+                        <tbody className='card-tbody'>
+                            <tr className='tr'>
+                                <td>
+                                    <p className='card-p'>Header 1</p>
+
+                                </td>
+                                <td><h1 class="card-header">h1. Luonvuituoi heading</h1></td>
+                            </tr>
+                            <tr className='tr'>
+                                <td>
+                                    <p className='card-p'>Header 2</p>
+
+                                </td>
+                                <td><h2 class="card-header">h2. Luonvuituoi heading</h2></td>
+                            </tr>
+                            <tr className='tr'>
+                                <td>
+                                    <p className='card-p'>Header 3</p>
+
+                                </td>
+                                <td><h3 class="card-header">h3. Luonvuituoi heading</h3></td>
+                            </tr>
+                            <tr className='tr'>
+                                <td>
+                                    <p className='card-p'>Header 4</p>
+
+                                </td>
+                                <td><h4 class="card-header">h4. Luonvuituoi heading</h4></td>
+                            </tr>
+                            <tr className='tr'>
+                                <td>
+                                    <p className='card-p'>Header 5</p>
+
+                                </td>
+                                <td><h5 class="card-header">h5. Luonvuituoi heading</h5></td>
+                            </tr>
+                            <tr className='tr'>
+                                <td>
+                                    <p className='card-p'>Header 6</p>
+
+                                </td>
+                                <td><h6 class="card-header">h6. Luonvuituoi heading</h6></td>
+                            </tr>
+                        </tbody>
+                    </table>
+       
+
+        </>
+    )
+}
