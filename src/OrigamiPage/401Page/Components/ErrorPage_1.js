4ae2f4868449d5167239048bebc692297f5f518d
vietanhle123 <93577622+vietanhle123@users.noreply.github.com>
2022-02-23 14:48:25 +0700

    table

--- src/OrigamiPage/401Page/Components/ErrorPage_1.js
+++ src/OrigamiPage/401Page/Components/ErrorPage_1.js
@@ -3,14 +3,14 @@ import '../Assets/Css/ErrorPage_1.css'
 
 import { Link } from 'react-router-dom';
 
-export default function PageNotFound() {
+export default function ErrorPage() {
     return (
         <>
             <div className='Header-401'>
             <br/>
 
                 <p className='ErrorPage'>401</p>
-                <h2 className='ErrorPage_2'></h2>
+                <h2 className='ErrorPage_2'>The page you're trying to access could not be loaded until you first log in with a valid user ID and password.!!!</h2>
                 <br/>
                 <Link to='/' className='PageNotFound-btn'> Home </Link>
             </div>
--- src/OrigamiPage/Table/Components/Table.js
+++ src/OrigamiPage/Table/Components/Table.js
@@ -3,14 +3,128 @@ import AdminNav from '../../Admin/Components/AdminNav'
 import '../Assets/Css/Table.css'
 
 function Table() {
-  return (
-    <>
-    <div>
-        <AdminNav />
-    </div>
-    <div></div>
-    </>
-  )
+
+    return (
+        <>
+            <div>
+
+                <AdminNav />
+
+                                            <div className='table-responsive'>
+
+                                                <div id='basic-datatables-wrapper' className='datatables-length-wrapper'>
+
+                                                    <div className='row_2'>
+
+                                                        <div className='table_2'>
+
+                                                            <div className='datatables-length' id='basic-datatables-length'>
+
+                                                                <label>
+
+                                                                    show
+
+                                                                    <select name='basic-datatables-length' aria-controls='basic-datatables' className='form-controls'>
+                                                                        <option value='1'></option>
+                                                                    </select>
+
+                                                                    entries
+
+                                                                </label>
+
+                                                            </div>
+
+                                                            <div className='datatables-fliter'>
+
+                                                                <label>
+                                                                    Search:
+                                                                    <input type='Search' className='form-control' placeholder aria-controls='basic-datatables'></input>
+                                                                </label>
+
+                                                            </div>
+
+                                                            <div className='datatables-col'>
+
+                                                                <table id='basic-datatables-btc' className='display table' role='grid' aria-describedby='basic-datatables-info'>
+
+                                                                    <thead>
+        
+                                                                        <tr role='row-3'>
+                                                                            <th className='vtn' tabIndex='0' aria-controls='basic-datatables' rowSpan='1' colSpan='1' >Name</th>
+                                                                            <th className='btc' tabIndex='0' aria-controls='basic-datatables' rowSpan='1' colSpan='1' >Position</th>
+                                                                            <th className='vnv' tabIndex='0' aria-controls='basic-datatables' rowSpan='1' colSpan='1' >Office</th>
+                                                                            <th className='unt' tabIndex='0' aria-controls='basic-datatables' rowSpan='1' colSpan='1' >Age</th>
+                                                                            <th className='vtn' tabIndex='0' aria-controls='basic-datatables' rowSpan='1' colSpan='1' >Start date</th>
+                                                                            <th className='add' tabIndex='0' aria-controls='basic-datatables' rowSpan='1' colSpan='1' >Salary</th>
+                                                                        </tr>
+        
+                                                                    </thead>
+
+
+                                                                    <tbody>
+
+                                                                        <tr role='row-4' className='TQM'>
+                                                                            <td className='phk'>Tạ Quang Minh</td>
+                                                                            <td>chief Executive Officer</td>
+                                                                            <td>Vietnam</td>
+                                                                            <td>19</td>
+                                                                            <td>???</td>
+                                                                            <td>1.000.000$</td>
+                                                                        </tr>
+
+                                                                        <tr role='row-5' classname='DTM'>
+                                                                            <td className='sor'>Đào Thành Nam</td>
+                                                                            <td>Software Engineer</td>
+                                                                            <td>Vietnam</td>
+                                                                            <td>18</td>
+                                                                            <td>???</td>
+                                                                            <td>1.000.000$</td>
+                                                                        </tr>
+
+                                                                        <tr role='row-6' className='VVV'>
+                                                                            <td className='ory'>Võ Văn Việt</td>
+                                                                            <td>Software Engineer</td>
+                                                                            <td>Vietnam</td>
+                                                                            <td>18</td>
+                                                                            <td>???</td>
+                                                                            <td>1.000.000$</td>
+                                                                        </tr>
+
+                                                                        <tr role='row-7' className='TMD'>
+                                                                            <td className='lkn'>Trần Minh Đức</td>
+                                                                            <td>Software Engineer</td>
+                                                                            <td>Vietnam</td>
+                                                                            <td>18</td>
+                                                                            <td>???</td>
+                                                                            <td>1.000.000$</td>
+                                                                        </tr>
+
+                                                                        <tr role='row-8' className='LNVA'>
+                                                                            <td className='don'>Lê Như Việt Anh</td>
+                                                                            <td>Software Engineer</td>
+                                                                            <td>Vietnam</td>
+                                                                            <td>18</td>
+                                                                            <td>???</td>
+                                                                            <td>1.000.000$</td>
+                                                                        </tr>
+                                                                    </tbody>
+
+                                                                </table>
+
+                                                            </div>
+
+                                                        </div>
+
+                                                    </div>
+
+                                                </div>
+
+                                            </div>
+
+                                        </div>
+
+        </>
+    );
 }
 
 export default Table
